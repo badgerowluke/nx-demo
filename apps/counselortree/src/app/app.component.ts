0548a8bdd65e88a9a629d5d41b54a311dd63a85f
Luke Badgerow <badgerow.luke@gmail.com>
2023-04-21 21:26:48 -0400

    touching up here for a brief moment.  This gets the counselor tree app to transpile and start with nx

--- apps/counselortree/src/app/app.component.ts
+++ apps/counselortree/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'swogeti-root',
   standalone: true,
+  imports: [ ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
